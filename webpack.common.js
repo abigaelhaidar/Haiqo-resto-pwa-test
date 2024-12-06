77884bb1d28b6f4fc4aead631e1d3b5e12e88b05
abigaelhaidar <abigaelhaidar@gmail.com>
2024-12-06 15:53:13 +0700

    comment bundler pack2

--- webpack.common.js
+++ webpack.common.js
@@ -5,7 +5,7 @@ const CopyWebpackPlugin = require('copy-webpack-plugin');
 const WorkboxWebpackPlugin = require('workbox-webpack-plugin');
 const ImageminWebpackPlugin = require('imagemin-webpack-plugin').default;
 const ImageminMozjpeg = require('imagemin-mozjpeg');
-const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+// const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 // Tambahkan BundleAnalyzerPlugin hanya jika dalam mode development
 // if (process.env.NODE_ENV === 'development') {
@@ -100,7 +100,7 @@ module.exports = {
       ],
     }),
 
-    new BundleAnalyzerPlugin(),
+    // new BundleAnalyzerPlugin(),
   ],
 };
 
